045f388a8c20f5389698d986a724c2ee1eb26a7b
MoonLo <moon_lo@yeah.net>
2018-05-24 15:11:10 +0800

    关于定时任务的配置

---
+++ SsmDemo/src/main/java/ssm/job/SsmJobClient.java
@@ -0,0 +1,26 @@
+package ssm.job;
+
+/**
+ * Copyright: Copyright (c) 2018
+ * Company: UD
+ *
+ * @author Uncle Drew
+ * @version 1.0 2018/5/24 14:57
+ */
+public class SsmJobClient {
+
+    /**
+     * 定时任务测试一
+     */
+    public void Task_1(){
+        System.out.println("这是第一个任务");
+    }
+
+    /**
+     * 定时任务测试一
+     */
+    public void Task_2(){
+        System.out.println("这是第二个任务");
+    }
+
+}
